642aa8d26a48bb2306fdc9e9c0c74645e7e9810a
Peter Makhnatch <p.makhnatch@gmail.com>
2022-12-07 21:14:32 -0500

    stable version

--- client.ts
+++ client.ts
@@ -4,7 +4,7 @@ import * as protoLoader from "@grpc/proto-loader";
 import { ProtoGrpcType } from "./proto/random";
 import readline from "readline";
 
-const PORT = 60543;
+const PORT = 50400;
 const PROTO_FILE = "./proto/random.proto";
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
@@ -28,15 +28,15 @@ client.waitForReady(deadline, (err) => {
 });
 
 function onClientReady() {
-  client.PingPong({ message: "Ping" }, (err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(result);
-  });
+  // client.PingPong({ message: "Ping" }, (err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(result);
+  // });
 
-  const stream1 = client.RandomNumbers({ maxVal: 85 });
+  const stream1 = client.RandomNumbers({ maxVal: 100000 });
   stream1.on("data", (chunk) => {
     console.log(chunk);
   });
@@ -44,46 +44,46 @@ function onClientReady() {
     console.log("communication ended");
   });
 
-  const stream2 = client.TodoList((err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(result);
-  });
-  stream2.write({ todo: "walk the wife", status: "Never" });
-  stream2.write({ todo: "walk the dog", status: "Doing" });
-  stream2.write({ todo: "get a real job", status: "Impossible" });
-  stream2.write({ todo: "feed the dog", status: "Done" });
-  stream2.end();
+  // const stream2 = client.TodoList((err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(result);
+  // });
+  // stream2.write({ todo: "walk the wife", status: "Never" });
+  // stream2.write({ todo: "walk the dog", status: "Doing" });
+  // stream2.write({ todo: "get a real job", status: "Impossible" });
+  // stream2.write({ todo: "feed the dog", status: "Done" });
+  // stream2.end();
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  // });
 
-  const username = process.argv[2];
-  if (!username) console.error("No username, can't join chat"), process.exit();
+  // const username = process.argv[2];
+  // if (!username) console.error("No username, can't join chat"), process.exit();
 
-  const metadata = new grpc.Metadata();
-  metadata.set("username", username);
-  const call = client.Chat(metadata);
+  // const metadata = new grpc.Metadata();
+  // metadata.set("username", username);
+  // const call = client.Chat(metadata);
 
-  call.write({
-    message: "register",
-  });
+  // call.write({
+  //   message: "register",
+  // });
 
-  call.on("data", (chunk) => {
-    console.log(`${chunk.username} ==> ${chunk.message}`);
-  });
+  // call.on("data", (chunk) => {
+  //   console.log(`${chunk.username} ==> ${chunk.message}`);
+  // });
 
-  rl.on("line", (line) => {
-    if (line === "quit") {
-      call.end();
-    } else {
-      call.write({
-        message: line,
-      });
-    }
-  });
+  // rl.on("line", (line) => {
+  //   if (line === "quit") {
+  //     call.end();
+  //   } else {
+  //     call.write({
+  //       message: line,
+  //     });
+  //   }
+  // });
 }
